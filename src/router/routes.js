22d2d9a2312c6b4cd355e6c76212a7de64bd1aa8
Pedro Vinicius Carvalho Ferreira dos Santos <pedro.ferreira@jbatista.com.br>
2024-01-12 13:24:14 -0300

    remove page

--- src/router/routes.js
+++ src/router/routes.js
@@ -10,7 +10,7 @@ const routes = [
       { path: 'email-confirmation', name: 'email-confirmation', component: () => import('pages/EmailConfirmationPage.vue') },
       { path: 'forgot-password', name: 'forgot-password', component: () => import('pages/ForgotPasswordPage.vue') },
       { path: 'reset-password', name: 'reset-password', component: () => import('pages/ResetPasswordPage.vue') },
-      { path: 'product-public/:id', name: 'product-public', component: () => import('pages/product/Public.vue') }
+      // { path: 'product-public/:id', name: 'product-public', component: () => import('pages/product/Public.vue') }
     ]
   },
   {
@@ -18,11 +18,11 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: 'me', name: 'me', component: () => import('pages/MePage.vue') },
-      { path: 'category', name: 'category', component: () => import('pages/category/List.vue') },
-      { path: 'form-category/:id?', name: 'form-category', component: () => import('pages/category/Form.vue') },
-      { path: 'product', name: 'product', component: () => import('pages/product/List.vue') },
-      { path: 'form-product/:id?', name: 'form-product', component: () => import('pages/product/Form.vue') },
-      { path: 'form-config/:id?', name: 'form-config', component: () => import('pages/config/Form.vue') }
+      // { path: 'category', name: 'category', component: () => import('pages/category/List.vue') },
+      // { path: 'form-category/:id?', name: 'form-category', component: () => import('pages/category/Form.vue') },
+      // { path: 'product', name: 'product', component: () => import('pages/product/List.vue') },
+      // { path: 'form-product/:id?', name: 'form-product', component: () => import('pages/product/Form.vue') },
+      // { path: 'form-config/:id?', name: 'form-config', component: () => import('pages/config/Form.vue') }
       // { path: '', component: () => import('pages/Index.vue') }
     ],
     meta: {
